315c9a8c09b8f8831a05060d65d9b50b5f472fbf
Siddharth Kundu <sk31395@gmail.com>
2024-07-28 01:48:02 -0400

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: 'IntelliPDF',
+  description: "IntelliPDF - Get your PDF answers from AI",
   icons: {
     icon: [
       {
